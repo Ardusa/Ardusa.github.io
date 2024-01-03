077ba68edb0ca6f2ef7db12c79edfba488f14109
Ardusa <ardusa05@gmail.com>
2024-01-03 11:43:51 -0500

    ignore loggythings2

---
+++ loggythings2/src/main/java/com/robostangs/loggythings/LoggyPrintCommand.java
@@ -0,0 +1,22 @@
+public class LoggyPrintCommand extends InstantCommand {
+    public LoggyPrintCommand(double toLog) {
+        super(() -> DataLogManager.log(Double.toString(toLog)));
+    }
+
+    public LoggyPrintCommand(int toLog) {
+        super(() -> DataLogManager.log(Integer.toString(toLog)));
+    }
+
+    public LoggyPrintCommand(Object toLog) {
+        super(() -> DataLogManager.log(toLog.toString()));
+    }
+
+    public LoggyPrintCommand(String toLog) {
+        super(() -> DataLogManager.log(toLog));
+    }
+
+    @Override
+    public boolean runsWhenDisabled() {
+        return true;
+    }
+}
